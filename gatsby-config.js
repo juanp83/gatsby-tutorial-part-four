8255600f7cedcffa72ad64c4c423e9f31686ec0a
Juan Portillo <juan@goowee.io>
2019-12-27 13:32:25 -0700

    wire up graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,17 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
-module.exports = {
-  /* Your site config here */
-}
+ module.exports = {
+   siteMetadata: {
+    title: `Pandas Eating Lots`,
+  },
+   plugins: [
+     `gatsby-plugin-emotion`,
+     {
+       resolve: `gatsby-plugin-typography`,
+       options: {
+         pathToConfigModule: `src/utils/typography`,
+       },
+     },
+   ],
+ }
