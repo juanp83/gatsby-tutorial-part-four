2ee5fd5d2a6e2d239c49077b50c7b182fa16368b
Juan Portillo <juan@goowee.io>
2019-12-27 17:30:57 -0700

    seo

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 export default ({ data }) => (
   <Layout>
+    <SEO title="about" />
     <h1>About {data.site.siteMetadata.title}</h1>
     <p>
       We're the only site running on your computer dedicated to showing the best
